55185d45852f3d04144416a4f9d8567d4e3c9966
skqureshnawaz <140653855+skqureshnawaz@users.noreply.github.com>
2023-07-27 01:21:30 +0530

    POC App
    POC App

---
+++ build/precache-manifest.eeea7237e170bc63830e800757e80406.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f6b5ade05a54e94c26d",
+    "url": "/static/css/main.966a9f03.chunk.css"
+  },
+  {
+    "revision": "8f6b5ade05a54e94c26d",
+    "url": "/static/js/main.8f6b5ade.chunk.js"
+  },
+  {
+    "revision": "975f63afe5057efb9a20",
+    "url": "/static/js/1.975f63af.chunk.js"
+  },
+  {
+    "revision": "43df66b859fe4959ceae",
+    "url": "/static/js/2.43df66b8.chunk.js"
+  },
+  {
+    "revision": "b9842c9c4c236a614e5d",
+    "url": "/static/js/runtime~main.b9842c9c.js"
+  },
+  {
+    "revision": "212055e9c9517af3b1401cf624634c07",
+    "url": "/index.html"
+  }
+];
